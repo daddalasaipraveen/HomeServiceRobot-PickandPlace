e5468857ea55040646e4b6234dc0b16bed956330
Sai Praveen Daddala <89826843+daddalasaipraveen@users.noreply.github.com>
2023-02-26 04:38:53 -0500

    Update add_markers.sh

--- catkin_ws/src/scripts/add_markers.sh
+++ catkin_ws/src/scripts/add_markers.sh
@@ -8,17 +8,17 @@
 
 
 # Defining workspace variable
-path_catkin_ws="/home/workspace/Project5/catkin_ws"
+#path_catkin_ws="/home/workspace/Project5/catkin_ws"
 
 
-xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"/home/workspace/Project5/catkin_ws/src/map/world/myworld.world\"" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"/home/workspace/Project5/catkin_ws/src/map/world/myworld.world\"" &
 sleep 5
 
-xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=\"/home/workspace/Project5/catkin_ws/src/map/map.yaml\"" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=\"/home/workspace/Project5/catkin_ws/src/map/map.yaml\"" &
 sleep 5
 
-xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 #rviz_path:=\"/home/workspace/Project4-MapMyWorld/catkin_ws/src/rvizconfig/rviz_marker_conf.rviz\"
 
-xterm -e "cd ${path_catkin_ws}; source devel/setup.bash; rosrun add_markers add_markers"
+xterm -e "source devel/setup.bash; rosrun add_markers add_markers"
