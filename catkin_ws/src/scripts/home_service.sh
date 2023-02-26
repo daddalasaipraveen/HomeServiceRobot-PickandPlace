24456ed1852ee7d5a56eca9014507c79f3ff4ae0
Sai Praveen Daddala <89826843+daddalasaipraveen@users.noreply.github.com>
2023-02-26 04:34:53 -0500

    Update home_service.sh

--- catkin_ws/src/scripts/home_service.sh
+++ catkin_ws/src/scripts/home_service.sh
@@ -15,7 +15,7 @@ sleep 5
 xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=\"/home/workspace/Project4-MapMyWorld/catkin_ws/src/map/map.yaml\"" &
 sleep 5
 
-xterm -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch rviz_path:=\"/home/workspace/Project4-MapMyWorld/catkin_ws/src/rvizconfig/rviz_marker_conf.rviz\"" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch rviz_path:=\"/home/workspace/Project4-MapMyWorld/catkin_ws/src/rvizconfig/rvizConf.rviz\"" &
 sleep 5
 
 xterm -e "source devel/setup.bash; rosrun add_markers add_markers" &
